c3e36d5b893f4edd7f6f7676cbb065c0be7c147c
Xingxin HE <xingxin.he@connect.ust.hk>
2026-03-23 22:31:07 +0800

    build: deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/phineuro-kimi/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
